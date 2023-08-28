105343a9ed18346b4f00da6fbf34948e5d7eb77b
guilherme <guilherme.bertolino.8762@ga.ita.br>
2023-08-28 09:45:42 -0300

    feat: create intelbras spider template

---
+++ spiders/intelbras.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy import Spider
+from scrapy import Request
+import sys, os
+import urllib.parse, urllib.request
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from download_firmware import parse_link
+from vendor_links import INTELBRAS
+
+class IntelbrasSpider(Spider):
+    name = "intelbras-spider"
+    start_urls = [INTELBRAS]
+
+    def parse(self, response):
+        pass
+
+    def parse_model(self, response):
+        pass
--- vendor_links.py
+++ vendor_links.py
@@ -1,2 +1,3 @@
 DLINK = "https://support.dlink.com"
 TPLINK_EN = "https://www.tp-link.com"
+INTELBRAS = "https://www.intelbras.com/pt-br/ajuda-download/download/"
