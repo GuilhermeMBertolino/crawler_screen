7356e2f6225d334eed34a9aaf77277de1708262a
guilherme <guilherme.bertolino.8762@ga.ita.br>
2024-01-12 12:30:56 -0300

    feat: add scrapy splash compatibility

--- settings.py
+++ settings.py
@@ -3,3 +3,14 @@ SPIDER_MODULES = ["spiders"]
 LOG_LEVEL = "INFO"
 LOG_FORMAT = "%(levelname)s: %(message)s"
 HTTPERROR_ALLOWED_CODES = [404]
+
+SPLASH_URL = "http://localhost:8050"
+DOWNLOAD_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
