02525b6a2c46fd57f56d1b0023fceb9057e2f208
guilherme <guilherme.bertolino.8762@ga.ita.br>
2024-03-25 17:33:07 -0300

    refactor: use scrapy_selenium instead of selenium

--- spiders/netgear.py
+++ spiders/netgear.py
@@ -1,6 +1,8 @@
 import scrapy
 from scrapy import Spider
 from scrapy import Request
+from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.by import By
 import sys, os
 import urllib.parse, urllib.request
 import re
@@ -18,28 +20,29 @@ class NetgearSpider(Spider):
     def parse(self, response):
         PRODUCT_SELECTOR = "//div[@class='list-view product_buy_info']"
         MODEL_SELECTOR = ".//a/text()"
+        MODEL_CODE_PATTERN = r"\((.*?)\)"
         products = response.xpath(PRODUCT_SELECTOR)
 
         self.logger.info(f"Start crawling {len(products)} pages")
 
         for product in products:
             model = product.xpath(MODEL_SELECTOR).extract_first()
-            model_code = re.search(r"\((.*?)\)", model).group()[1:-1]
-            yield Request(
+            model_code = re.search(MODEL_CODE_PATTERN, model).group()[1:-1]
+            yield SeleniumRequest(
                 url=urllib.parse.urljoin(NETGEAR, f"/support/download/?model={model_code}"),
                 meta={"model_name": model_code},
-                callback=self.parse_model
+                callback=self.parse_model,
+                wait_time=2
             )
 
     def parse_model(self, response):
-        FIRMWARE_LINK_SELECTOR = "//p[contains(text(), 'Firmware')]/.."      
+        FIRMWARE_LINK_SELECTOR = "//p[contains(text(), 'Firmware')]/../@href"      
 
-        driver.get(response.url)
-        download_link = driver.find_element(By.XPATH, FIRMWARE_LINK_SELECTOR)
+        download_link = response.xpath(FIRMWARE_LINK_SELECTOR)
 
         if download_link:
             self.logger.info(f"Downloading firmware for {response.meta['model_name']}")
-            parse_link(download_link.get_attribute("href"), "netgear", response.meta["model_name"])
+            parse_link(download_link.extract_first(), "netgear", response.meta["model_name"])
 
         else:
             self.logger.info(f"No firmware available for {response.meta['model_name']}")
