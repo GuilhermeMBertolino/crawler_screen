9d80dc7f7fc4b5b9a5876d6452ae8b00df32ce8d
guilherme <guilherme.bertolino.8762@ga.ita.br>
2024-05-08 11:23:00 -0300

    feat: finish intelbras crawler, yet to add start_urls

--- spiders/intelbras.py
+++ spiders/intelbras.py
@@ -4,6 +4,7 @@ from scrapy import Request
 from time import sleep
 import sys, os
 import urllib.parse, urllib.request
+import requests
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -12,24 +13,25 @@ from vendor_links import INTELBRAS
 
 def get_base_url(url):
     parsed_url = urllib.parse.urlparse(url)
-    base_url = urllib.parse.urlunparse((parsed_url.scheme, parsed_url.netloc, "", "", "", ""))
-    return urllib.parse.urljoin(base_url, "/")
+    base_url = parsed_url._replace(path='/'.join(parsed_url.path.split('/')[:-1]))
+    return f"{urllib.parse.urlunparse(base_url)}/"
 
 class IntelbrasSpider(Spider):
     name = "intelbras-spider"
     start_urls = ["https://manual-bifrost.intelbras.com.br/index_ChangLogs.html",
-                  "https://manuais.intelbras.com.br/manual-linha-rx/index_ChangeLogs.html"]
+                  "https://manuais.intelbras.com.br/manual-linha-rx/index_ChangeLogs.html",
+                  ]
 
     def parse(self, response):
-        CONTAINER_SELECTOR = "//div[@class='menuManual']"
+        CONTAINER_SELECTOR = "//div[contains(@class, 'menuManual')]"
         LINK_SELECTOR = ".//a/@href"
         MODEL_SELECTOR = ".//p[@class='titleOptionMenuManual']/text()"
 
-        container = response.xpath(CONTAINER_SELECTOR).extract_first()
+        container = response.xpath(CONTAINER_SELECTOR)
 
         if container:
-            urls = response.xpath(LINK_SELECTOR)
-            models = response.xpath(MODEL_SELECTOR)
+            urls = container.xpath(LINK_SELECTOR)
+            models = container.xpath(MODEL_SELECTOR)
             self.logger.info(f"Start crawling {len(models)} pages")
 
             for url, model in zip(urls, models):
@@ -42,4 +44,17 @@ class IntelbrasSpider(Spider):
 
 
     def parse_model(self, response):
-        pass
+        LINK_SELECTOR = "//img[@id='download-box']/../@href"
+
+        download_link = response.xpath(LINK_SELECTOR)
+
+        if not download_link:
+            self.logger.info(f"No firmware available for {response.meta['model_name']}")
+            with open("intelbras.txt", "a") as f:
+                f.write(f"{response.meta['model_name']} - No firmware\n")
+            return
+        
+        self.logger.info(f"Downloading firmware for {response.meta['model_name']}")
+        url = urllib.parse.urljoin(get_base_url(response.url), download_link.extract_first())
+        with open("intelbras.txt", "a") as f:
+            f.write(f"{response.meta['model_name']} - {url}\n")
